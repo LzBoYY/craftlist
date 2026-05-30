702ec9ba234bedd1233f3f156d2e2c902820b8e8
LzBoYY <87152858+LzBoYY@users.noreply.github.com>
2026-05-30 11:35:54 +0200

    Update supabase.js

--- public/js/supabase.js
+++ public/js/supabase.js
@@ -1,1 +1,12 @@
 
+const supabaseUrl =
+  "https://umvpigxwceekndxnyrmi.supabase.co";
+
+const supabaseKey =
+  "sb_publishable_4UiI7fMJCoGKUnbhzwYkTg_0_r8F2Vz";
+
+window.supabaseClient =
+  window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+  );
