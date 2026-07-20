e2abdf90c9063e76574883afcd05767c3bf43eea
LzBoYY <87152858+LzBoYY@users.noreply.github.com>
2026-07-20 14:49:11 +0200

    Add Stripe webhook handler

---
+++ api/stripe-webhook.js
@@ -0,0 +1,17 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+
+  if (req.method !== "POST") {
+    return res.status(405).send("Method not allowed");
+  }
+
+  console.log("Webhook received");
+
+  return res.status(200).json({
+    received: true
+  });
+
+}
